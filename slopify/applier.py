9db56868478376525c5dc0758d39ad5916e1095a
Liam Moore <lmoore90@gmail.com>
2023-11-18 13:20:04 +0100

    fix some failing tests for apply with nested markdown

--- slopify/applier.py
+++ slopify/applier.py
@@ -1,61 +1,157 @@
 import typing as ty
 import argparse
+from pydantic import BaseModel
 from markdown_it import MarkdownIt
+from markdown_it.token import Token
 from pathlib import Path
 
 
 def unescape_code_blocks(content: str) -> str:
     """
-    Unescape code blocks by removing the unique HTML comments.
+    Unescapes escaped code blocks within Markdown content.
+
+    This function is used to handle the special case where code blocks within
+    Markdown files have been escaped to prevent conflicts with the Markdown syntax.
+
+    Args:
+        content (str): The Markdown content with potentially escaped code blocks.
+
+    Returns:
+        str: The Markdown content with code blocks unescaped.
     """
     return content.replace("<!--SLOPIFY_CODE_BLOCK```-->", "```")
 
 
+# def apply_markdown(markdown_content: str, base_path: ty.Optional[Path] = None):
+#     """
+#     Writes a set of files serialised as slopify markdown back to the file system.
+
+#     For every type of included file except markdown files, this just writes the code 
+#     blocks, which by construction represent the file content.
+
+#     For markdown files themselves (for example, README.md, CONTRIBUTING.md) which 
+#     are serialised inside slopify markdown, we have to deal with the case where 
+#     markdown content (represented by a markdown code block) must be written back 
+#     to a markdown file. This markdown code block may itself include
+#     code blocks which have been escaped by slopify upon serialisation. The full 
+#     contents of this markdown code block must be written back to a markdown file 
+#     with the code blocks unescaped.
+#     """
+#     md = MarkdownIt()
+#     tokens = md.parse(markdown_content)
+
+class FileContent(BaseModel):
+    """
+    A simple data structure to hold the path and content for a file.
+
+    Attributes:
+        path (Path): The path where the file should be written.
+        content (str): The content to be written to the file.
+    """
+    path: Path
+    content: str
+
+def parse_markdown(tokens: list[Token], base_path: Path) -> list[FileContent]:
+    """
+    Parses a list of Markdown tokens and extracts the file paths and contents.
+
+    This function assumes that each file's content is represented as a code block
+    immediately following an H1 heading with the file's relative path. 
+    For Markdown files, all content between file headers is included. It also
+    handles the special case of Markdown files that may contain nested code blocks,
+    which need to be unescaped before writing back to the file system.
+
+    Args:
+        tokens (list[Token]): A list of tokens obtained from parsing Markdown content.
+        base_path (Path): The base path relative to which file paths will be resolved.
+
+    Returns:
+        list[FileContent]: A list of FileContent objects with extracted paths and contents.
+    """
+    file_contents = []
+    current_path = None
+    current_content_lines = []
+    capture_markdown_content = False  # Flag to capture all content for Markdown files
+
+    for token in tokens:
+        if token.type == 'heading_open' and token.tag == 'h1':
+            # If we already have a path and content, save them before starting a new file
+            if current_path is not None:
+                content = '\n'.join(current_content_lines)
+                file_contents.append(FileContent(path=current_path, content=content))
+            current_path = None
+            current_content_lines = []
+            capture_markdown_content = False  # Reset the flag
+        elif token.type == 'inline' and current_path is None:
+            # Extract the file path from the inline token's content
+            current_path = base_path / token.content.strip('`')
+            # Check if the current file is a Markdown file
+            capture_markdown_content = current_path.suffix == '.md'
+        elif capture_markdown_content:
+            # Capture all content for Markdown files
+            if token.type == 'fence':
+                # For code blocks, capture the content including the backticks and language info
+                current_content_lines.append(unescape_code_blocks(token.content))#f"```{token.info}\n{token.content}```")
+            else:
+                # For non-code block content, capture the raw content
+                current_content_lines.append(token.content)
+        elif token.type == 'fence' and current_path is not None and not capture_markdown_content:
+            # For non-Markdown files, capture only the code block content
+            current_content_lines.append(token.content)
+
+    # Handle the last file content if any
+    if current_path is not None:
+        content = '\n'.join(current_content_lines)
+        file_contents.append(FileContent(path=current_path, content=content))
+
+    return file_contents
+
+def write_files(file_contents: list[FileContent]):
+    """
+    Writes the content of each FileContent object to the filesystem.
+
+    This function is responsible for creating any necessary directories and writing
+    the file content to the specified paths. It also handles the special case of
+    Markdown files, ensuring that any nested code blocks are correctly unescaped
+    before writing.
+
+    Args:
+        file_contents (list[FileContent]): A list of FileContent objects to be written.
+    """
+    for file_content in file_contents:
+        file_path = file_content.path
+        file_path.parent.mkdir(parents=True, exist_ok=True)  # Create directories if needed
+        with file_path.open('w', encoding='utf-8') as file:
+            file.write(file_content.content)
+
+
 def apply_markdown(markdown_content: str, base_path: ty.Optional[Path] = None):
+    """
+    Applies Markdown content to the filesystem, writing files as described in the content.
+
+    This function parses the Markdown content, extracts file paths and contents,
+    unescapes code blocks if necessary, and writes the content to the filesystem.
+    It handles the special case of Markdown files that may contain nested code blocks,
+    ensuring that they are correctly unescaped before writing.
+
+    Args:
+        markdown_content (str): The Markdown content to be applied.
+        base_path (Path, optional): The base directory for applying the code. Defaults to the current working directory.
+    """
     md = MarkdownIt()
     tokens = md.parse(markdown_content)
 
-    code_blocks = {}
-    markdown_sections = {}
-    current_file_path = None
-    in_markdown_file = False
-    markdown_accumulator = []
+    # Step 2: Refine the Parsing Logic
+    base_path = base_path or Path.cwd()
+    file_contents = parse_markdown(tokens, base_path)
 
-    for token in tokens:
-        if token.type == "heading_open" and token.tag == "h1":
-            # The next token is the text of the heading, which contains the file path
-            current_file_path = tokens[tokens.index(token) + 1].content.strip("`")
-            in_markdown_file = current_file_path.endswith(".md")
-            if in_markdown_file:
-                # Start accumulating Markdown content for .md files
-                markdown_accumulator = [current_file_path]
-        elif in_markdown_file:
-            # Accumulate all content for Markdown files
-            markdown_accumulator.append(token.content)
-        elif token.type == "fence" and current_file_path:
-            # This token contains the content of the code block
-            code_blocks[current_file_path] = token.content
-        else:
-            print(f"skipping token {token.content}")
+    # Step 4: Handle Special Cases
+    for file_content in file_contents:
+        if file_content.path.suffix == '.md':
+            file_content.content = unescape_code_blocks(file_content.content)
 
-    # Apply the code blocks to the file system
-    for file_path, code in code_blocks.items():
-        print(f"raw {code=}")
-        full_path = (base_path or Path.cwd()) / file_path
-        full_path.parent.mkdir(parents=True, exist_ok=True)
-        with full_path.open("w", encoding="utf-8") as f:
-            print(f"processed {code=}")
-            f.write(code)
-
-    # Apply the Markdown sections to the file system
-    for markdown_accumulator in markdown_sections.values():
-        file_path = markdown_accumulator[0]
-        markdown_content = "\n".join(markdown_accumulator[1:])
-        markdown_content = unescape_code_blocks(markdown_content)
-        full_path = (base_path or Path.cwd()) / file_path
-        full_path.parent.mkdir(parents=True, exist_ok=True)
-        with full_path.open("w", encoding="utf-8") as f:
-            f.write(markdown_content)
+    # Step 3: Verify File Writing Logic
+    write_files(file_contents)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Apply Markdown file to directory.")
--- tests/test_applier.py
+++ tests/test_applier.py
@@ -1,5 +1,5 @@
 import pytest
-from slopify.applier import apply_markdown
+from slopify.applier import *
 from pathlib import Path
 
 @pytest.fixture
@@ -71,3 +71,108 @@ print('Subdir file')
     subdir_file = tmp_path / "nested" / "subdir" / "subdir_file.py"
     assert nested_file.read_text() == "print('Nested file')\n"
     assert subdir_file.read_text() == "print('Subdir file')\n"
+
+
+
+# Test for parsing Markdown with nested code blocks
+def test_parse_markdown_nested_code_blocks():
+    """
+    Test that parse_markdown correctly identifies and extracts the content
+    for Markdown files with nested code blocks, ensuring that the code blocks
+    are marked for unescaping.
+    """
+    # Markdown content with nested code blocks
+    markdown_content = """
+# `nested_markdown.md`
+
+```
+# Example Markdown Content
+
+This is a Markdown file with nested code blocks.
+
+<!--SLOPIFY_CODE_BLOCK```-->python
+# This is a nested code block
+print("Hello, nested world!")
+<!--SLOPIFY_CODE_BLOCK```-->
+```
+"""
+    md = MarkdownIt()
+    tokens = md.parse(markdown_content)
+    base_path = Path.cwd()
+    file_contents = parse_markdown(tokens, base_path)
+    # There should be one FileContent object
+    assert len(file_contents) == 1
+    # The path should be correct
+    assert file_contents[0].path == base_path / "nested_markdown.md"
+    # The content should include the marker for unescaping
+    assert "<!--SLOPIFY_CODE_BLOCK" not in file_contents[0].content
+    assert "```" in file_contents[0].content
+
+# Test for writing files with nested Markdown content
+def test_write_files_nested_markdown():
+    """
+    Test that write_files correctly writes the content for Markdown files,
+    including unescaping any nested code blocks.
+    """
+    # FileContent with nested code blocks marked for unescaping
+    file_content = FileContent(
+        path=Path("nested_markdown.md"),
+        content="""
+# Example Markdown Content
+
+This is a Markdown file with nested code blocks.
+
+<!--SLOPIFY_CODE_BLOCK\`\`\`python-->
+# This is a nested code block
+print("Hello, nested world!")
+<!--SLOPIFY_CODE_BLOCK\`\`\`-->
+"""
+    )
+    write_files([file_content])
+    # Read the file and check the content
+    written_content = Path("nested_markdown.md").read_text()
+    assert "```python" in written_content
+    assert "print(\"Hello, nested world!\")" in written_content
+
+# Test for unescaping code blocks within Markdown content
+def test_unescape_code_blocks():
+    """
+    Test that unescape_code_blocks correctly unescapes escaped code blocks
+    within Markdown content.
+    """
+    escaped_content = """
+This is a Markdown file.
+
+<!--SLOPIFY_CODE_BLOCK\`\`\`python-->
+# This is a nested code block
+print("Hello, nested world!")
+<!--SLOPIFY_CODE_BLOCK\`\`\`-->
+"""
+    unescaped_content = unescape_code_blocks(escaped_content)
+    assert "```python" in unescaped_content
+    assert "<!--SLOPIFY_CODE_BLOCK" not in unescaped_content
+
+# Test for the full apply_markdown process with nested Markdown
+def test_apply_markdown_with_nested_markdown(tmp_path):
+    """
+    Test that apply_markdown correctly processes Markdown content with nested
+    Markdown files, unescaping code blocks and writing the files to the filesystem.
+    """
+    markdown_content = """
+# `nested_markdown.md`
+
+```
+# Example Markdown Content
+
+This is a Markdown file with nested code blocks.
+
+\`\`\`python
+# This is a nested code block
+print("Hello, nested world!")
+\`\`\`
+```
+"""
+    apply_markdown(markdown_content, base_path=tmp_path)
+    written_content = (tmp_path / "nested_markdown.md").read_text()
+    assert "```python" in written_content
+    assert "print(\"Hello, nested world!\")" in written_content
